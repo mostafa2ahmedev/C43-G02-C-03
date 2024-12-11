55d08cc0d1ab396d32f1a2f62d6f48302ca816d2
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-12 01:28:10 +0200

    Q2

--- Assingment/Program.cs
+++ Assingment/Program.cs
@@ -14,8 +14,22 @@ namespace Assingment
             //    Console.WriteLine("Yes");
             //else
             //    Console.WriteLine("No");
-                
+
             #endregion
+
+            #region Q2)Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.Write("Enter a Number please: ");
+            //int.TryParse(Console.ReadLine(), out int Number);
+
+            //if (Number < 0)
+            //    Console.WriteLine("Negative");
+            //else 
+            //    Console.WriteLine("Positive");
+
+
+
+            #endregion
+
         }
     }
 }
