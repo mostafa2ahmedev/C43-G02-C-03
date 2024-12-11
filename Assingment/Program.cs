b3e0ee98bd70dcf55f9b73426be9bbbb1d8c483d
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-12 01:21:39 +0200

    Q1

---
+++ Assingment/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assingment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1)Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+            //Console.Write("Enter a Number please: ");
+            //int.TryParse(Console.ReadLine(),out int Number);
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+                
+            #endregion
+        }
+    }
+}
